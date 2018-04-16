50f4ab4b7e7afddfe11d76645a753f8633424d77
dsriniv1 <sdwarak8877@gmail.com>
2018-04-16 15:48:28 +0530

    adding varibles

--- SpringBootDemo/src/main/java/com/cg/controller/RequestController.java
+++ SpringBootDemo/src/main/java/com/cg/controller/RequestController.java
@@ -9,6 +9,12 @@ import org.springframework.web.bind.annotation.RestController;
 	
 	private String v1;
 	
+	private String v2;
+	
+	private String v3;
+	
+	private String v4;
+	
 	
 	@RequestMapping("/")
 	public String requestMethod(){
