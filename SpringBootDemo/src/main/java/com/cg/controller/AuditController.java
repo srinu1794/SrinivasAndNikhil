68c2fbd7912b3deb9a20f3901a9a3e83b5f06990
dsriniv1 <sdwarak8877@gmail.com>
2018-04-16 15:55:24 +0530

    adding AuditController

---
+++ SpringBootDemo/src/main/java/com/cg/controller/AuditController.java
@@ -0,0 +1,22 @@
+package com.cg.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+
+public class AuditController {
+	
+	
+	
+	private String var1;
+	
+	private String var2;
+	
+	
+	@RequestMapping("/allResponses")
+	public void responseMethod() {
+		
+		
+		
+	}
+	
+
+}
