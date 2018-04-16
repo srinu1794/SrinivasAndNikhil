d64a8b831a4dc5655f58cae9920e682b0d69f730
srinu1794 <sdwarak8877@gmail.com>
2018-04-16 16:05:32 +0530

    remove the varibles

--- SpringBootDemo/src/main/java/com/cg/controller/RequestController.java
+++ SpringBootDemo/src/main/java/com/cg/controller/RequestController.java
@@ -9,11 +9,7 @@ import org.springframework.web.bind.annotation.RestController;
 	
 	private String v1;
 	
-	private String v2;
 	
-	private String v3;
-	
-	private String v4;
 	
 	
 	@RequestMapping("/")
